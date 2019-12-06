016f00e48874c83d8687b5ca3dbcbc66e30286ad
lavyun <lavyun@163.com>
2019-12-06 18:51:46 +0800

    chore: remove unused function and script.

--- dist/mock.browser.esm.js
+++ dist/mock.browser.esm.js
@@ -11,40 +11,6 @@ var constant = {
     RE_PLACEHOLDER: /\\*@([^@#%&()\?\s]+)(?:\((.*?)\))?/g
 };
 
-/*! *****************************************************************************
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
-
-THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-MERCHANTABLITY OR NON-INFRINGEMENT.
-
-See the Apache Version 2.0 License for specific language governing permissions
-and limitations under the License.
-***************************************************************************** */
-
-var __assign = function() {
-    __assign = Object.assign || function __assign(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-
-function __spreadArrays() {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-}
-
 /* type-coverage:ignore-next-line */
 var type = function (value) {
     return isDef(value)
@@ -71,12 +37,6 @@ var isRegExp = function (value) {
 };
 var isFunction = function (value) {
     return type(value) === 'function';
-};
-var isObjectOrArray = function (value) {
-    return isObject(value) || isArray(value);
-};
-var isNumeric = function (value) {
-    return !isNaN(parseFloat(value)) && isFinite(value);
 };
 var keys = function (obj) {
     var keys = [];
@@ -113,13 +73,6 @@ var heredoc = function (fn) {
         .replace(/[\s\xA0]+$/, ''); // .trim()
 };
 var noop = function () { };
-var logInfo = function () {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    console.log.apply(console, __spreadArrays(['[better-mock]'], args));
-};
 var assert = function (condition, error) {
     if (!condition) {
         throw new Error('[better-mock] ' + error);
@@ -154,16 +107,47 @@ var Util = /*#__PURE__*/Object.freeze({
   isArray: isArray,
   isRegExp: isRegExp,
   isFunction: isFunction,
-  isObjectOrArray: isObjectOrArray,
-  isNumeric: isNumeric,
   keys: keys,
   values: values,
   heredoc: heredoc,
   noop: noop,
-  logInfo: logInfo,
   assert: assert,
   createCustomEvent: createCustomEvent
 });
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+
+function __spreadArrays() {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+}
 
 var MAX_NATURE_NUMBER = 9007199254740992;
 var MIN_NATURE_NUMBER = -9007199254740992;
@@ -8104,27 +8088,6 @@ var Assert = {
             return true;
         }
         result.push(Assert.createDiffResult(type, path, actual, expected, message, 'matches'));
-        return false;
-    },
-    notEqual: function (type, path, actual, expected, result, message) {
-        if (actual !== expected) {
-            return true;
-        }
-        result.push(Assert.createDiffResult(type, path, actual, expected, message, 'is not equal to'));
-        return false;
-    },
-    greaterThan: function (type, path, actual, expected, result, message) {
-        if (actual > expected) {
-            return true;
-        }
-        result.push(Assert.createDiffResult(type, path, actual, expected, message, 'is greater than'));
-        return false;
-    },
-    lessThan: function (type, path, actual, expected, result, message) {
-        if (actual < expected) {
-            return true;
-        }
-        result.push(Assert.createDiffResult(type, path, actual, expected, message, 'is less to'));
         return false;
     },
     greaterThanOrEqualTo: function (type, path, actual, expected, result, message) {
