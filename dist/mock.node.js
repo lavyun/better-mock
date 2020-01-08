f2b1bb194fb6061cfdb49f8d12ef4d3724a7d39c
lavyun <lavyun@163.com>
2020-01-08 14:09:05 +0800

    feat: release 0.2.2

--- dist/mock.node.js
+++ dist/mock.node.js
@@ -1,5 +1,5 @@
 /*!
-  * better-mock v0.2.1 (mock.node.js)
+  * better-mock v0.2.2 (mock.node.js)
   * (c) 2019-2020 lavyun@163.com
   * Released under the MIT License.
   */
@@ -508,7 +508,7 @@ var helper = /*#__PURE__*/Object.freeze({
 // image
 // 常见图片尺寸
 var imageSize = [
-    '150x100', '300x200', '400x300', '600x450', '800X600',
+    '150x100', '300x200', '400x300', '600x450', '800x600',
     '100x150', '200x300', '300x400', '450x600', '600x800',
     '100x100', '200x200', '300x300', '450x450', '600x600'
 ];
@@ -8142,7 +8142,7 @@ var Mock = {
     valid: valid,
     mock: mock,
     heredoc: heredoc,
-    version: '0.2.1'
+    version: '0.2.2'
 };
 // Mock.mock( template )
 // 根据数据模板生成模拟数据。
